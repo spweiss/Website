330700e29398dda71a678538ce564e306ffd6793
spweiss <spweiss@seas.upenn.edu>
2018-05-25 23:12:10 -0400

    Complete animation

--- app/assets/javascripts/typewriter.js
+++ app/assets/javascripts/typewriter.js
@@ -1,7 +1,12 @@
 var i = 0;
 var j = 0;
+var k = 0;
+var l = 0;
+var m = 0;
 var txt = 'Spencer Weiss';
-var speed = 125;
+var msg = 'Hi there!';
+var inv = 'Scroll down...';
+var speed = 100;
 var delay = 2000;
 
 function typeWriter() {
@@ -19,5 +24,35 @@ function untype() {
     document.getElementById("type").innerHTML = document.getElementById("type").innerHTML.slice(0, -1);
     j++;
     setTimeout(untype, speed);
+  } else {
+    setTimeout(welcome, delay);
+  }
+}
+
+function welcome() {
+  if (k < msg.length) {
+    document.getElementById("type").innerHTML += msg.charAt(k);
+    k++;
+    setTimeout(welcome, speed);
+  } else {
+    setTimeout(untype2, delay);
+  }
+}
+
+function untype2() {
+  if (l < msg.length) {
+    document.getElementById("type").innerHTML = document.getElementById("type").innerHTML.slice(0, -1);
+    l++;
+    setTimeout(untype2, speed);
+  } else {
+    setTimeout(invite, delay);
+  }
+}
+
+function invite() {
+  if (m < inv.length) {
+    document.getElementById("type").innerHTML += inv.charAt(m);
+    m++;
+    setTimeout(invite, speed);
   }
 }
