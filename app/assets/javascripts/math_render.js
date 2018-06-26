a7cbcbde8c440e9b4098b08a0058204acd89b32d
spweiss <spweiss@seas.upenn.edu>
2018-06-26 15:53:20 -0400

    Update gemfile

--- app/assets/javascripts/math_render.js
+++ app/assets/javascripts/math_render.js
@@ -1,8 +1,1 @@
-require katex/katex
 
-function math_render() {
-  var math = document.getElementsByClassName("math");
-  for (var i = 0; i < math.length; i++) {
-    katex.render(math[i].innerHTML, math[i]);
-  }
-}
