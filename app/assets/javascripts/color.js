406f5a1d96898d9c51574cac02430c0f4cef0163
spweiss <spweiss@seas.upenn.edu>
2018-07-17 13:35:03 -0400

    Set jQuery colors to only run on the homepage

--- app/assets/javascripts/color.js
+++ app/assets/javascripts/color.js
@@ -1,28 +1,30 @@
 $(function() {
    $(window).scroll(function () {
-      if ($(this).scrollTop() > $(".page").height() * 0.70) {
-         $("div").addClass("changeColorWhite")
-      }
-      if ($(this).scrollTop() < $(".page").height() * 0.70) {
-         $("div").removeClass("changeColorWhite")
-      }
-      if ($(this).scrollTop() > $(".page").height() * 1.70) {
-         $("div").addClass("changeColorTan")
-      }
-      if ($(this).scrollTop() < $(".page").height() * 1.70) {
-         $("div").removeClass("changeColorTan")
-      }
-      if ($(this).scrollTop() > $(".page").height() * 2.70) {
-         $("div").addClass("changeColorGrey")
-      }
-      if ($(this).scrollTop() < $(".page").height() * 2.70) {
-         $("div").removeClass("changeColorGrey")
-      }
-      if ($(this).scrollTop() > $(".page").height() * 3.70) {
-         $("div").addClass("changeColorCyan")
-      }
-      if ($(this).scrollTop() < $(".page").height() * 3.70) {
-         $("div").removeClass("changeColorCyan")
+      if (top.location.pathname === "/") {
+        if ($(this).scrollTop() > $(".page").height() * 0.70) {
+           $("div").addClass("changeColorWhite")
+        }
+        if ($(this).scrollTop() < $(".page").height() * 0.70) {
+           $("div").removeClass("changeColorWhite")
+        }
+        if ($(this).scrollTop() > $(".page").height() * 1.70) {
+           $("div").addClass("changeColorTan")
+        }
+        if ($(this).scrollTop() < $(".page").height() * 1.70) {
+           $("div").removeClass("changeColorTan")
+        }
+        if ($(this).scrollTop() > $(".page").height() * 2.70) {
+           $("div").addClass("changeColorGrey")
+        }
+        if ($(this).scrollTop() < $(".page").height() * 2.70) {
+           $("div").removeClass("changeColorGrey")
+        }
+        if ($(this).scrollTop() > $(".page").height() * 3.70) {
+           $("div").addClass("changeColorCyan")
+        }
+        if ($(this).scrollTop() < $(".page").height() * 3.70) {
+           $("div").removeClass("changeColorCyan")
+        }
       }
    });
 });
